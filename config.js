61459f0e92eeabc79278da568911bd197f56aa57
Rayyan <knightscarrier@gmail.com>
2023-02-18 03:30:51 +1100

    Version 3

--- config.js
+++ config.js
@@ -1,2 +1,5 @@
 console.log("config.js");
 console.log("version1");
+//New changes in this code
+
+console.log("Hello World!!! This is Git Practice.");
--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 console.log("index.js");
 console.log("version1");
+
+console.log("Enter Two Numbers.");
+
+let numA;
+let numB;
+
+let sum = numA + numB;
+
+console.log(sum);
